da40d1f23a75739a891154f563a6280b780a9beb
Monika Brandt <monika.brandt@medsci.uu.se>
2019-03-01 13:18:08 +0100

    Code clean-up and re-organization

--- bin/get_qc_config.py
+++ bin/get_qc_config.py
@@ -6,48 +6,57 @@ import argparse
 import yaml
 
 
-def get_mapping_handlers():
-    multiqc_mapping = {'ClusterPFHandler':'total', #bcl2fastq
-                       'ErrorRateHandler': 'Error', #Interop
-                       'Q30Handler': 'percent_Q30', #bcl2fastq
-                       'ReadsPerSampleHandler': 'mqc-generalstats-bcl2fastq-total'} #bcl2fastq
-
-    return multiqc_mapping
-
-def get_qc_criteria(mapper,checkqc_config):#This is ugly, checkq_config is passed through convert_to_multiqc_config. Maybe it should be global in a class.
-    for handler in checkqc_config:
-        if handler['name'] == mapper:
-            return(handler)
-
-def get_qc_level(mapper):
-    #This is where we should get lt or gt.
-    warning_level = {'ClusterPFHandler':'lt',
-                     'ErrorRateHandler': 'gt',
-                     'Q30Handler': 'lt',
-                     'ReadsPerSampleHandler': 'lt'}
-
-    return warning_level[mapper]
+class ValueHandlerMapper(object):
+    def __init__(self, handler_name, multiqc_mapping, compare_direction):
+        self.handler_name = handler_name
+        self.multiqc_mapping = multiqc_mapping
+        self.compare_direction = compare_direction
 
 
-def convert_to_multiqc_config(checkqc_config):
-    mapping_handlers = get_mapping_handlers()
-    #I dont't want to return the whole dictionary from get_mapping_handlers, but I have to iterate over it.
-    #Can I do this differently? It would be prefferd to iterate over the checkqc_config, but I don't want to use all keys
-    #so then I have to make the code handle exceptions instead. Or should it be a global variable in an __init__ together with a config-file?
+class HandlerMapper():
+    def __init__(self):
+        self._mapper_list = [ValueHandlerMapper(handler_name = 'ClusterPFHandler',
+                                                multiqc_mapping = 'total',
+                                                compare_direction = 'lt'),
+                             ValueHandlerMapper(handler_name = 'ErrorRateHandler',
+                                                multiqc_mapping = 'Error',
+                                                compare_direction = 'gt'),
+                             ValueHandlerMapper(handler_name = 'Q30Handler',
+                                                multiqc_mapping = 'percent_Q30',
+                                                compare_direction = 'lt'),
+                             ValueHandlerMapper(handler_name = 'ReadsPerSampleHandler',
+                                                multiqc_mapping = 'mqc-generalstats-bcl2fastq-total',
+                                                compare_direction = 'lt')]
+
+        self.mapping = self._convert_to_mappings(self._mapper_list)
+
+    def _convert_to_mappings(self, mapper_list):
+        mapper_dict = {}
+        for mapper in mapper_list:
+            mapper_dict[mapper.handler_name] = mapper
+        return mapper_dict
+
+def convert_to_multiqc_config(checkqc_config_dict):
     multiqc_config_format = {}
-    for mapper in mapping_handlers:
-        multiqc_name = mapping_handlers[mapper]
-        qc_criteria = get_qc_criteria(mapper,checkqc_config)
-        qc_level = get_qc_level(mapper)
-        multiqc_config_value = {multiqc_name: {}}
+    handler_mapper = HandlerMapper()
+    for mapper_name, mapper in handler_mapper.mapping.items():
+        qc_criteria = checkqc_config_dict.get(mapper.handler_name)
+        multiqc_config_value = {mapper.multiqc_mapping: {}}
         if not qc_criteria['warning'] == 'unknown':
-            multiqc_config_value[multiqc_name]['warn'] = [{qc_level: qc_criteria['warning']}]
+            multiqc_config_value[mapper.multiqc_mapping]['warn'] = [{mapper.compare_direction: qc_criteria['warning']}]
         if not qc_criteria['error'] == 'unknown':
-            multiqc_config_value[multiqc_name]['error'] = [{qc_level: qc_criteria['error']}]
+            multiqc_config_value[mapper.multiqc_mapping]['error'] = [{mapper.compare_direction: qc_criteria['error']}]
 
-        multiqc_config_format[multiqc_name] = multiqc_config_value[multiqc_name]
+        multiqc_config_format[mapper.multiqc_mapping] = multiqc_config_value[mapper.multiqc_mapping]
 
     return {'table_cond_formatting_rules': multiqc_config_format}
+
+def convert_to_dict(checkqc_config):
+    checkqc_config_dict = {}
+    for qc_handler in checkqc_config:
+        checkqc_config_dict[qc_handler['name']] = qc_handler
+
+    return checkqc_config_dict
 
 
 if __name__ == "__main__":
@@ -66,7 +75,8 @@ if __name__ == "__main__":
     both_read_lengths = run_type_recognizer.read_length()
     read_length = int(both_read_lengths.split("-")[0])
     checkqc_config = config.get_handler_configs(instrument_and_reagent_version, read_length)
-    multiqc_config = convert_to_multiqc_config(checkqc_config)
+    checkqc_config_dict = convert_to_dict(checkqc_config)
+    multiqc_config = convert_to_multiqc_config(checkqc_config_dict)
 
     with open('qc_thresholds.yaml', 'w') as outfile:
         yaml.dump(multiqc_config, outfile)
